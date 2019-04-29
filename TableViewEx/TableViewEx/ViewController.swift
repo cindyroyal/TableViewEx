9cf7ec192abd80a8be67e42102c58294a281f687
Cindy Royal <cindy@codeactually.com>
2019-04-29 12:01:45 -0500

    Update ViewController.swift

--- TableViewEx/TableViewEx/ViewController.swift
+++ TableViewEx/TableViewEx/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
     var newsItems = ["News 1", "News 2", "News 3"]
+    @IBOutlet weak var theNews: UITableView!
+
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return newsItems.count
@@ -33,41 +35,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             performSegue(withIdentifier: "News3Segue", sender: self)
         }
     }
-    //Additional TableView functions to allow for editing/deleting
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            theNews.beginUpdates()
-            theNews.deleteRows(at: [indexPath], with: .automatic)
-            newsItems.remove(at: indexPath.row)
-            theNews.endUpdates()
-            
-        }
-    }
-
-    
-
-    
-
-    
-    @IBOutlet weak var theNews: UITableView!
-    
-    
-    
-    
+   
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         theNews.delegate = self
         theNews.dataSource = self
-    theNews.allowsMultipleSelectionDuringEditing = true
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
